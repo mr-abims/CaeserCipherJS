06f025fffc658d98494f6bda1f81d38cd331c153
ABIMS707 <adebayoabimbola9@gmail.com>
2021-12-21 05:51:58 +0100

    Cipher JS

---
+++ cipher.js
@@ -0,0 +1,26 @@
+const alphabet = ['A','B','C', 'D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+function rot9(str) {
+    // create placeholder
+    let placeHolder = '';
+
+    // loop through the string
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+        const isALetter = alphabet.includes(char);
+        // if char is not letter, add to placeHolder
+        if (isALetter === false) {
+            placeHolder += char;
+        } else {
+            // else, rotate + or - 9, add to placeHolder
+            const charIndex = alphabet.findIndex((c) => c === char);
+
+            placeHolder += alphabet[charIndex  + 9] || alphabet[charIndex - 9];
+
+        }
+    }
+    return placeHolder;
+}
+
+// Test the encryption with these
+console.log (rot9('THIS IS A GOOD DAY'))
